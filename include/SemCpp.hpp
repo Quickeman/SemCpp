56b3e9ee8737d1b633e59615633b23af2649af36
Quickeman <finnquicke@hotmail.com>
2022-06-16 12:30:15 +0100

    Separated API into binary and counting classes

--- include/SemCpp.hpp
+++ include/SemCpp.hpp
@@ -3,8 +3,49 @@
 
 #include <chrono>
 
-/** Semaphore class. */
-class Semaphore {
+/** Binary semaphore class.
+ * Any call to @ref wait will acquire all previous @ref post calls. */
+class BinarySemaphore {
+public:
+    /** Clock type used for the @ref wait_for and @ref wait_until methods. */
+    using clock_t = std::chrono::steady_clock;
+
+    /** Constructor. */
+    BinarySemaphore(bool init = false);
+
+    /** Destructor. Default. */
+    ~BinarySemaphore() = default;
+
+    /** Posts semaphore availability.
+     * @returns the previous state of the semaphore. */
+    bool post() noexcept;
+
+    /** Checks for semaphore availability.
+     * Returns immediately with `true` if available, `false` otherwise. */
+    bool check() const noexcept;
+
+    /** Waits for the semaphore to become available. */
+    void wait() noexcept;
+
+    /** Waits for the semaphore to become available for @a time.
+     * If the semaphore becomes available before @a time has elapsed, returns
+     * `true`, otherwise returns `false`. */
+    bool wait_for(std::chrono::seconds time);
+
+    /** Waits for the semaphore to become available until @a time.
+     * If the semaphore becomes available before @a time, returns `true`,
+     * otherwise returns `false`. */
+    bool wait_until(std::chrono::time_point<clock_t> time);
+
+private:
+    /** Semaphore availability flag. */
+    bool _flag;
+};
+
+
+/** Counting semaphore class.
+ * Each @ref post adds an availability to the semaphore. */
+class CountingSemaphore {
 public:
     /** Type used for post count. */
     using count_t = unsigned;
@@ -13,10 +54,10 @@ public:
     using clock_t = std::chrono::steady_clock;
 
     /** Constructor. */
-    Semaphore(count_t init = 0);
+    CountingSemaphore(count_t init = 0);
 
     /** Destructor. Default. */
-    ~Semaphore() = default;
+    ~CountingSemaphore() = default;
 
     /** Posts semaphore availability. */
     count_t post(count_t n = 1) noexcept;
--- src/SemCpp.cpp
+++ src/SemCpp.cpp
@@ -1,32 +1,72 @@
 #include <SemCpp.hpp>
 
-Semaphore::Semaphore(count_t i):
+// class BinarySemaphore
+
+BinarySemaphore::BinarySemaphore(bool i):
+_flag{i} {}
+
+bool BinarySemaphore::post() noexcept {
+    const auto p {this->_flag};
+    this->_flag = true;
+    return p;
+}
+
+bool BinarySemaphore::check() const noexcept {
+    return this->_flag;
+}
+
+void BinarySemaphore::wait() noexcept {
+    while(!this->check()) {}
+
+    // Lower flag
+    this->_flag = false;
+}
+
+bool BinarySemaphore::wait_for(std::chrono::seconds t) {
+    return this->wait_until(clock_t::now() + t);
+}
+
+bool BinarySemaphore::wait_until(std::chrono::time_point<clock_t> t) {
+    do {
+        if (this->check()) {
+            this->_flag = false;
+            return true;
+        }
+    } while (clock_t::now() < t);
+
+    return false;
+}
+
+
+// class CountingSemaphore
+
+CountingSemaphore::CountingSemaphore(count_t i):
 _count{i} {}
 
-Semaphore::count_t Semaphore::post(count_t n) noexcept {
+CountingSemaphore::count_t CountingSemaphore::post(count_t n) noexcept {
     this->_count += n;
 }
 
-Semaphore::count_t Semaphore::count() const noexcept {
+CountingSemaphore::count_t CountingSemaphore::count() const noexcept {
     return this->_count;
 }
 
-bool Semaphore::check() const noexcept {
+bool CountingSemaphore::check() const noexcept {
     return this->_count > 0;
 }
 
-void Semaphore::wait() noexcept {
+void CountingSemaphore::wait() noexcept {
     while(!this->check()) {}
 
     // Decrement counter when semaphore becomes available
     this->_count--;
 }
 
-bool Semaphore::wait_for(std::chrono::seconds t) {
+bool CountingSemaphore::wait_for(std::chrono::seconds t) {
     return this->wait_until(clock_t::now() + t);
 }
 
-bool Semaphore::wait_until(std::chrono::time_point<clock_t> t) {
+bool CountingSemaphore::wait_until(std::chrono::time_point<clock_t> t) {
     do {
         if (this->check()) {
             this->_count--;
